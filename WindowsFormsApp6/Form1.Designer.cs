0431cd756b319d3a97668ad24b4188afafc9d143
gummoni <shaty3@hotmail.com>
2020-11-21 20:58:23 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace WebView2HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 45);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webView21
+            // 
+            this.webView21.CreationProperties = null;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 45);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(800, 405);
+            this.webView21.Source = new System.Uri("https://www.microsoft.com", System.UriKind.Absolute);
+            this.webView21.TabIndex = 1;
+            this.webView21.Text = "webView21";
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace WebView2HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        //https://docs.microsoft.com/ja-jp/microsoft-edge/webview2/concepts/distribution
+        //https://qiita.com/NagaJun/items/4925a63ce7b93b80639e
+
+        public Form1()
+        {
+            InitializeComponent();
+            webView21.NavigationCompleted += WebView21_NavigationCompleted;
+        }
+
+        JsToCs CsClass = new JsToCs();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (webView21 != null && webView21.CoreWebView2 != null)
+            {
+                webView21.CoreWebView2.NavigateToString(File.ReadAllText("page\\index.html"));
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await CallJS();
+        }
+
+        private void WebView21_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (webView21?.CoreWebView2 != null)
+            {
+                webView21.CoreWebView2.AddHostObjectToScript("aaa", CsClass);
+            }
+        }
+
+        async Task CallJS()
+        {
+            var str1 = await webView21.ExecuteScriptAsync("func1(\"C#からの呼び出し\")");
+            Debug.WriteLine(str1);
+        }
+    }
+
+    //[ClassInterface(ClassInterfaceType.AutoDual)]
+    //[ComVisible(true)]
+    public class JsToCs
+    {
+        public void MessageShow(string strText)
+        {
+            MessageBox.Show("JSからの呼び出し:" + strText);
+        }
+    }
+}
